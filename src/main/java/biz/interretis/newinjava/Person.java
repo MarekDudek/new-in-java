8267e24304a56860c75ee96600d961503b0381dd
Marek Dudek <marek.dudek@gmail.com>
2014-12-28 20:16:05 +0100

    Removed javadoc

--- src/main/java/biz/interretis/newinjava/Person.java
+++ src/main/java/biz/interretis/newinjava/Person.java
@@ -2,67 +2,45 @@ package biz.interretis.newinjava;
 
 import java.time.LocalDate;
 
-/**
- * Person.
- * 
- * @author marek
- */
 public final class Person {
 
-    /**
-     * Sex.
-     * 
-     * @author marek
-     */
     public enum Sex {
         MALE, FEMALE
     }
 
-    /** Name. */
     private String name;
-    /** Birthday. */
     private LocalDate birthday;
-    /** Gender. */
     private Sex gender;
-    /** E-mail address. */
     private String emailAddress;
 
-    /** Name getter. */
     public String getName() {
         return name;
     }
 
-    /** Name setter. */
     public void setName(final String aName) {
         this.name = aName;
     }
 
-    /** Birthday getter. */
     public LocalDate getBirthday() {
         return birthday;
     }
 
-    /** Birthday setter. */
     public void setBirthday(final LocalDate aBirthday) {
         this.birthday = aBirthday;
     }
 
-    /** Gender getter. */
     public Sex getGender() {
         return gender;
     }
 
-    /** Gender setter. */
     public void setGender(final Sex aGender) {
         this.gender = aGender;
     }
 
-    /** E-mail getter. */
     public String getEmailAddress() {
         return emailAddress;
     }
 
-    /** E-mail setter. */
     public void setEmailAddress(final String aEmailAddress) {
         this.emailAddress = aEmailAddress;
     }
