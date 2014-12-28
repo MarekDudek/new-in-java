3877dc57e165c687118a112f4a610a05018b0c06
Marek Dudek <marek.dudek@gmail.com>
2014-12-28 20:07:47 +0100

    Tabs changed to spaces

--- src/main/java/biz/interretis/newinjava/Person.java
+++ src/main/java/biz/interretis/newinjava/Person.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 public final class Person {
 
     public enum Sex {
-	MALE, FEMALE
+        MALE, FEMALE
     }
 
     private String name;
@@ -14,34 +14,34 @@ public final class Person {
     private String emailAddress;
 
     public String getName() {
-	return name;
+        return name;
     }
 
     public void setName(final String aName) {
-	this.name = aName;
+        this.name = aName;
     }
 
     public LocalDate getBirthday() {
-	return birthday;
+        return birthday;
     }
 
     public void setBirthday(final LocalDate aBirthday) {
-	this.birthday = aBirthday;
+        this.birthday = aBirthday;
     }
 
     public Sex getGender() {
-	return gender;
+        return gender;
     }
 
     public void setGender(final Sex aGender) {
-	this.gender = aGender;
+        this.gender = aGender;
     }
 
     public String getEmailAddress() {
-	return emailAddress;
+        return emailAddress;
     }
 
     public void setEmailAddress(final String aEmailAddress) {
-	this.emailAddress = aEmailAddress;
+        this.emailAddress = aEmailAddress;
     }
 }
