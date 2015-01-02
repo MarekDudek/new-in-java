20ad121dcf0027dc9fb32e303fabc7275026cfe4
Marek Dudek <marek.dudek@gmail.com>
2015-01-02 12:27:16 +0100

    The same but with lambda

--- src/test/java/biz/interretis/newinjava/futures/CompletableFuturesTest.java
+++ src/test/java/biz/interretis/newinjava/futures/CompletableFuturesTest.java
@@ -3,13 +3,13 @@ package biz.interretis.newinjava.futures;
 import static biz.interretis.newinjava.futures.ComputationsSimulator.ANSWER_TO_ULTIMATE_QUESTION;
 import static biz.interretis.newinjava.futures.ComputationsSimulator.doSomeLongComputation;
 import static biz.interretis.newinjava.futures.ComputationsSimulator.doSomethingElse;
+import static java.util.concurrent.Executors.newCachedThreadPool;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -22,7 +22,7 @@ public class CompletableFuturesTest {
     public void executing_long_lasting_operation_succeeds__pre_java_8() throws Exception {
 
         // given
-        final ExecutorService executor = Executors.newCachedThreadPool();
+        final ExecutorService executor = newCachedThreadPool();
 
         // when
         final Future<Double> future = executor.submit(new Callable<Double>() {
@@ -45,7 +45,7 @@ public class CompletableFuturesTest {
     public void executing_long_lasting_operation_fails__pre_java_8() throws Exception {
 
         // given
-        final ExecutorService executor = Executors.newCachedThreadPool();
+        final ExecutorService executor = newCachedThreadPool();
 
         // when
         final Future<Double> future = executor.submit(new Callable<Double>() {
@@ -54,6 +54,23 @@ public class CompletableFuturesTest {
                 return doSomeLongComputation(1000L);
             }
         });
+
+        doSomethingElse(500L);
+
+        // then
+        assertThat(future.isDone(), is(false));
+
+        future.get(1, TimeUnit.MILLISECONDS);
+    }
+
+    @Test(expected = TimeoutException.class)
+    public void executing_long_lasting_operation_fails__with_lambda() throws Exception {
+
+        // given
+        final ExecutorService executor = newCachedThreadPool();
+
+        // when
+        final Future<Double> future = executor.submit(() -> doSomeLongComputation(1000L));
 
         doSomethingElse(500L);
 
